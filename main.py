7903d11bdb588f7a8f2b9eb6cb33910b20b3d27d
oskar <oskarxpd@gmail.com>
2022-02-03 13:59:13 +0100

    repair leds

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-COLORS = {'red': (255, 0, 0), 'blue': (0, 0, 255)}
+COLORS = {'red': (255, 0, 0), 'blue': (0, 0, 255), 'magenta': (255, 0, 255)}
 
 @app.get("/color")
 async def color(color: str):
@@ -21,4 +21,17 @@ async def color(color: str):
         print(f'set color to {set_color}')
         return {"message": "success"}
     except Exception as e:
+        print(e)
+        return {"error": e}
+
+@app.get("/rgb")
+async def rgb(r: int, g: int, b:int):
+    try:
+        set_rgb = (r, g, b)
+        pixel.fill(set_rgb)
+        pixel.write()
+        print(f'set color to {set_rgb}')
+        return {"message": "success"}
+    except Exception as e:
+        print(e)
         return {"error": e}
