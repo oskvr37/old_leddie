16291c2d8c2031b6e9ae24532194ee00ae3c97f7
oskvr37 <oskarxpd@gmail.com>
2022-02-09 12:32:51 +0100

    remove old files

--- config.py
+++
@@ -1,9 +0,0 @@
-import board
-import neopixel
-
-PIN = board.D18
-LED_COUNT = 86
-
-pixel = neopixel.NeoPixel(board.D18, 86)
-
-COLORS = {'red': (255, 0, 0), 'blue': (0, 0, 255), 'green': (0, 255, 0), 'magenta': (255, 0, 255), 'white': (255, 255, 255)}
--- leds.py
+++
@@ -1,4 +0,0 @@
-from config import pixel
-
-pixel.fill((255,255,255))
-pixel.write()
