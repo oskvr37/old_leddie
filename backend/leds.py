153255008b936675e540b2062feb0753a1a74665
oskar <oskarxpd@gmail.com>
2022-02-10 20:08:13 +0100

    add smooth fill

--- backend/leds.py
+++ backend/leds.py
@@ -1,7 +1,7 @@
 import board
 import neopixel
 
-from modes import hue_rgb
+import modes
 
 
 PIN = board.D18
@@ -11,14 +11,17 @@ LED_COUNT = 86
 class Pixel:
     def __init__(self) -> None:
         self.pixel = neopixel.NeoPixel(board.D18, 86, auto_write=False)
+        self.color = (0, 0, 0)
         self.startup()
 
     def startup(self):
         for hue in range(0, 361):
-            self.fill(hue_rgb(hue))
+            self.pixel.fill(modes.hue_rgb(hue))
+            self.pixel.write()
         self.fill((0, 0, 0))
 
     def fill(self, rgb):
-        self.color = rgb
-        self.pixel.fill(rgb)
-        self.pixel.write()
+        for color in modes.smooth_rgb(self.color, rgb):
+            self.pixel.fill(color)
+            self.pixel.write()
+        self.color = color
--- backend/modes.py
+++ backend/modes.py
@@ -38,3 +38,21 @@ def hue_rgb(hue):
     else:
         return 255, 255, 255
     return int(r), int(g), int(b)
+
+
+def smooth_rgb(rgb_1, rgb_2):
+    SMOOTH_STEP = 10
+    def fade(one, two):
+        lower, reverse = one, False
+        if one > two: lower, reverse = two, True
+        step = abs(one - two) / SMOOTH_STEP
+        arr = [int(round(s * step + lower, 0)) for s in range(1, 10)]
+        if reverse:
+            return arr[::-1] + [lower]
+        return arr + [two]
+
+    r1, g1, b1 = rgb_1
+    r2, g2, b2 = rgb_2
+    r, g, b = fade(r1, r2), fade(g1, g2), fade(b1, b2)
+    rgb_list = zip(r, g, b)
+    return rgb_list
