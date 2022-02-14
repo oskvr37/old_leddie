2b8d07d81e089439bcfdf51edc66c1f071079362
oskvr37 <oskarxpd@gmail.com>
2022-02-14 21:26:07 +0100

    remove nginx

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../backend
-uvicorn main:app --reload --host 0.0.0.0 --port 8081
+uvicorn main:app --reload --host 0.0.0.0 --port 8000
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -d -m -S leddie sh backend.sh & screen -d -m -S website sh backend.sh
+screen -d -m -S leddie sh backend.sh & screen -d -m -S website sh frontend.sh
